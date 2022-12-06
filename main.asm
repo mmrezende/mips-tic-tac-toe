c055d7a659691bac51c32e5bb08509f6e2130156
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-12-06 16:19:23 -0300

    better organize the procedures

--- main.asm
+++ main.asm
@@ -1,17 +1,42 @@
 .data
-	board: .ascii "\0\0\0\0\0\0\0\0\0\0" # 3x3 board, storing the current game state as characters
-	turn: .byte 1 # Who's turn is to play (1 for 'X' and 0 for 'O')
+	boardX: .byte 0 # byte storing the positions marked as X
+	boardO: .byte 0 # byte storing the positions marked as O
 .text
 	jal main # Call the main procedure (start the program)
 	
-	# Store the value 17 in the $v0 register (exit2 syscall)
-	# set $a0 to 0
+	# Set $a0 to the return value of main
+	# set $v0 register to 17 (exit2 syscall)
 	# and issue a system call
-	# (exit the program successfully)
+	move $a0, $v0
 	li $v0, 17
-	li $a0, 0
 	syscall
-	# Procedure to verify if the game has finished (either someone won or it's a draw)
+	
+	# User interaction to get the player's move
+	# Args: NONE
+	# Return value: NONE
+	player_input:
+	
+		jr $ra
+	
+	# AI Logic to choose it's move
+	# Args: NONE
+	# Return value: NONE
+	bot_play:
+	
+		jr $ra
+	
+	# Procedure that modifies the board according to who's turn is to play
+	# Args: 
+	# $a0 - isXTurn (Who's turn is to play)
+	# $a1 - isBotX (if bot plays as X)
+	# Return value: NONE
+	# Stack:
+	#
+	play:
+	
+		jr $ra
+	
+	# Logic to verify if the game has finished (either someone won or it's a draw)
 	# Args: NONE
 	# Return value: $v0
 	# 0 - Game is still running
@@ -26,46 +51,67 @@
 	print_board:
 		for_1:
 	
+		end_for_1:
 		jr $ra
 	
-	print_turn:
-		jal print_board
-		jr $ra
-	# Prints a message according to how the game ended
-	# Args: $a0
+	# Prints a message according to the game state
+	# Args: $a0 - boardState
 	# Return value: NONE
-	print_result:
+	print_turn:
 		# TODO
 		# should be a switch-case using $a0
+		jal print_board
 		
 		jr $ra
 	
 	# Main procedure
-	# Stack:
-	# 0 - 4 | $ra
 	# Args: NONE
-	# Result Value: NONE
+	# Return value: $v0 - exit2 status
+	# Stack:
+	# 12 - 16 | isXTurn (Who's turn is to play)
+	# 8 - 12  | isBotX (if bot plays as X)
+	# 4 - 8   | boardState (return value of evaluate_board)
+	# 0 - 4   | $ra (return address)
 	main:
 	# Prologue
-		# Increase stack by 4 bytes
-		addiu $sp, $sp, -4
+		# Increase stack
+		addiu $sp, $sp, -16
 		# Store $ra at $sp
 		sw $ra, ($sp)
 	
 	# Body
+		# TODO: generates a pseudo-random boolean to decide who starts the game
+		# defaults to player first
+		sw $zero, 8($sp)
+		
+		# isXTurn = TRUE (X starts the game)
+		li $t0, 1
+		sw $t0, 12($sp)
+	
 		# Main Game Loop
 		main_loop:
 			jal evaluate_board
-			bnez $v0, end_main_loop
+			sw $v0, 4($sp)
+			move $a0, $v0
+			jal print_result
+			
+			# Break loop if game has ended (boardState=0)
+			lw $t0, 4($sp)
+			bnez $t0, end_main_loop
+			
+			# Otherwise, play
+			# play(isXTurn, isBotX)
+			lw $a0, 12($sp)
+			lw $a1, 8($sp)
+			jal play
+			
+			j main_loop
 		end_main_loop:
-		
-		move $a0, $v0
-		jal print_result
 		
 	# Epilogue
 		# Restore $ra value
 		lw $ra, ($sp)
-		# Reduce stack by 4 bytes
-		addiu $sp, $sp, 4
+		# Reduce the stack
+		addiu $sp, $sp, 16
 		
 		jr $ra
